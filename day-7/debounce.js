b935e430cc42dd9e2e5976c45fe09443e6afa1fa
glitch86 <mdpolokahmed825@gmail.com>
2026-06-15 19:50:12 +0600

    solved debounce

---
+++ day-7/debounce.js
@@ -0,0 +1,10 @@
+function debounce(fn, delay) {
+  let timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+}
+
